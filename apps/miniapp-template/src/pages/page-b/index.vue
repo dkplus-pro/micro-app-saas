74a890fc517d70b2688c07d7ad7f2e5e750d55a8
dk <dkplus@qq.com>
2026-07-09 14:10:20 +0800

    omx(team): auto-checkpoint worker-1 [1]

--- apps/miniapp-template/src/pages/page-b/index.vue
+++ apps/miniapp-template/src/pages/page-b/index.vue
@@ -1,5 +1,7 @@
-<script lang="ts">
-import BizPage from '../../biz/pages/page-b/index.vue';
+<template>
+  <BizPage />
+</template>
 
-export default BizPage;
+<script setup lang="ts">
+import BizPage from '../../biz/pages/page-b/index.vue';
 </script>
